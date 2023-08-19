031a426a0abe241b03d6a5e8a622a9ad4707f404
Md-arefin <ansariarefin617@gmail.com>
2023-08-19 22:55:24 +0600

    update navber

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -39,13 +39,21 @@ const Navbar = () => {
             .catch(err => console.log(err))
     }
 
-
-
-
+    // Stricky Navbar
     const handleScroll = () => {
-        setIsScrolled(window.screenY >= 20);
-    }
-
+      if (window.scrollY > 100) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+  
+    useEffect(() => {
+      window.addEventListener('scroll', handleScroll);
+      return () => {
+        window.removeEventListener('scroll', handleScroll);
+      };
+    }, []);
 
     useEffect(() => {
         window.addEventListener('scroll', handleScroll);
@@ -56,8 +64,9 @@ const Navbar = () => {
     }, [])
 
     return (
-        <div className={`max-w-screen-2xl navbar  fixed z-10 w-[91%] ${isScrolled ? 'bg-black text-white' : "bg-transparent"} `}>
-            <div className="navbar-start  relative">
+       
+        <div className={`${isScrolled ? ' bg-slate-500 sticky top-0 w-[91%] lg:w-full' : 'bg-transparent -top-24'} max-w-screen-2xl navbar transition duration-300 ease-in-out z-10`} style={{transition: 'all 0.3 ease'}}>
+            <div className="navbar-start relative">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
@@ -84,10 +93,10 @@ const Navbar = () => {
                         </div>
                     </div>
 
-                    <Link onClick={handleLogOut} className="btn bg-rose-800 text-white font-bold hover:text-black ml-5">Logout</Link>
+                    <Link onClick={handleLogOut} className="btn bg-rose-800 border-none text-white font-bold hover:text-black ml-5">Logout</Link>
                 </>
                     :
-                    <Link to='login' className="btn bg-rose-800 text-white font-bold hover:text-black">Login</Link>
+                    <Link to='login' className="btn bg-rose-800 text-white font-bold border-none hover:text-black">Login</Link>
                 }
             </div>
         </div>
